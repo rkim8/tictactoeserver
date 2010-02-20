0af2e3b39ee4700ad36869af05886e86e11809be
ingovals@gmail.com <ingovals@gmail.com@d42e8890-1b01-11df-a558-f1789200f0e9>
2010-02-20 18:19:32 +0000

    Code

---
+++ trunk/src/is/ru/cs/tsam/consoletictactoe/Client.java
@@ -0,0 +1,75 @@
+package is.ru.cs.tsam.consoletictactoe;
+
+import java.io.*;
+import java.net.*;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Client {
+
+	private Socket connection;
+	private DataInputStream input;
+	private DataOutputStream output;
+
+	public void connect(String address){
+		try {
+			connection = new Socket(address, 55000);
+			input = new DataInputStream(connection.getInputStream());
+			output = new DataOutputStream(connection.getOutputStream());
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+			System.out.println("Server not found");
+			System.exit(0);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		System.out.println("Connection successful");
+
+
+		try {
+			int nPlayer = input.readInt();
+			if (nPlayer == 0)
+				System.out.println("You are player X, waiting for other player");
+			else System.out.println("You are player O");
+
+
+			while(input.readBoolean()) {
+				System.out.println(input.readUTF());
+				System.out.println("Choose a square to mark");
+				boolean b = false;
+				do {
+					Scanner in = new Scanner(System.in);
+					try{
+						output.writeInt(in.nextInt());
+						b = false;
+					}
+					catch (InputMismatchException e) {
+						System.out.println("Please insert a valid number");
+						b = true;
+					}
+				}
+				while(b);
+				if(input.readBoolean()) {
+					System.out.println(input.readUTF());
+					System.out.println("Wait for other player");
+				}
+				else {
+					System.out.println("Invalid move try again");
+				}
+			}
+			int winner = input.readInt();
+			if (winner == nPlayer) System.out.println("Congratulation you won");
+			else System.out.println("You suck");
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public static void main(String[] args) {
+		Client c = new Client();
+		c.connect("127.0.0.1");
+	}
+
+}
---
+++ trunk/src/is/ru/cs/tsam/consoletictactoe/Game.java
@@ -0,0 +1,74 @@
+package is.ru.cs.tsam.consoletictactoe;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+public class Game extends Thread {
+
+	private DataInputStream[] input;
+	private DataOutputStream[] output;
+	private Socket[] player;
+	private TicTacToe game;
+
+
+	public Game() {
+		player = new Socket[2];
+		input = new DataInputStream[2];
+		output = new DataOutputStream[2];
+		game = new TicTacToe();
+	}
+
+	public void addPlayer(Socket connection) {
+		int i;
+		if (player[0] == null)
+			i = 0;
+		else if (player[1] == null)
+			i = 1;
+		else return;
+		player[i] = connection;
+		try{
+			input[i] = new DataInputStream(connection.getInputStream());
+			output[i] = new DataOutputStream(connection.getOutputStream());
+			output[i].writeInt(i);
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}	
+	}
+
+	public void run() {
+		boolean isPlaying = true;
+		int nPlayerTurn = 0;	
+
+		try {
+			output[nPlayerTurn].writeBoolean(true);
+			while(isPlaying) {
+				output[nPlayerTurn].writeUTF(game.toString());
+				int nSquare = input[nPlayerTurn].readInt();
+				if (game.markSquare(nSquare, nPlayerTurn)) {
+					output[nPlayerTurn].writeBoolean(true);
+					output[nPlayerTurn].writeUTF(game.toString());
+					int winner = game.getWinner();
+					if (winner != -1) {
+						isPlaying = false;
+						output[nPlayerTurn].writeBoolean(isPlaying);
+					}
+					nPlayerTurn = (nPlayerTurn +1) % 2;
+					output[nPlayerTurn].writeBoolean(isPlaying);
+				}
+				else {
+					output[nPlayerTurn].writeBoolean(false);
+					output[nPlayerTurn].writeBoolean(true);				
+				}
+			}
+			output[0].writeInt(game.getWinner());
+			output[1].writeInt(game.getWinner());
+		}
+		catch( IOException e) {
+			e.printStackTrace(  );
+			System.exit( 1 );
+		}
+	}
+}
