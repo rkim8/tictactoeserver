2fefd707727cb76569cf4f587ab414f236e3a1fb
ingovals@gmail.com <ingovals@gmail.com@d42e8890-1b01-11df-a558-f1789200f0e9>
2010-02-22 13:41:15 +0000

    Added viewer, untested atm

---
+++ trunk/src/is/ru/cs/tsam/consoletictactoe/ViewerListener.java
@@ -0,0 +1,53 @@
+package is.ru.cs.tsam.consoletictactoe;
+
+import java.io.*;
+import java.net.*;
+import java.util.ArrayList;
+
+public class ViewerListener extends Thread {
+
+	private ArrayList<Game> gameList;
+	private DatagramSocket viewerSocket;
+	private DatagramPacket packet;
+	private byte[] incoming;
+	private byte[] outgoing;
+
+	public ViewerListener(ArrayList<Game> list) {
+		gameList = list;
+		incoming = new byte[1];
+		try {
+			viewerSocket = new DatagramSocket(56000);
+			viewerSocket.setSoTimeout(10000);
+		} catch (SocketException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void run() {
+		while(true) {
+			packet = new DatagramPacket(incoming, incoming.length);
+			try {
+				viewerSocket.receive(packet);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}			
+			sendResponse((int)incoming[0], packet.getSocketAddress());
+		}	
+	}
+
+	public void sendResponse(int nGame, SocketAddress sAdd) {
+		try {
+			String gameStatus = gameList.get(nGame).getGame().toString();
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			DataOutputStream dos = new DataOutputStream(baos);
+			dos.writeUTF(gameStatus);
+			outgoing = baos.toByteArray();
+			packet = new DatagramPacket(outgoing, outgoing.length, sAdd);
+			viewerSocket.send(packet);
+		}
+		catch (IOException e){
+			e.printStackTrace();
+		}
+	}
+}
+
